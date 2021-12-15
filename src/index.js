1c48a934ec0731cd471b00f32039ec3a2d1e75a6
Mahdi-Soultana <soultana.mahdi24@gmail.com>
2021-12-15 11:26:30 -0800

    add

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const userRoute = require("./routes/userRoute");
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb" }));
