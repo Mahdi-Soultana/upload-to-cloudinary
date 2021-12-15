c7417a4e21ce273d319e5d17715fc47f07e2ef65
Mahdi-Soultana <soultana.mahdi24@gmail.com>
2021-12-15 10:54:36 -0800

    build

--- src/index.js
+++ src/index.js
@@ -9,10 +9,10 @@ app.use(express.urlencoded({ limit: "50mb" }));
 app.use("/user", userRoute);
 
 app.use(express.static(__dirname + "/public"));
-
+const PORT = process.env.PORT || 3000;
 app.get("/", (req, res) => {
   res.send({
     data: "home  page",
   });
 });
-app.listen(3000, () => console.log("server runing in port 3000"));
+app.listen(PORT, () => console.log("server runing in port 3000"));
